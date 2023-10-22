9a35bd1febc20bc6ec391027533bfa5ca3fe7ee6
abdullahnz <edfallennzm@gmail.com>
2023-10-22 15:08:30 +0700

    add `answer`, `question_bank`, and `choice` migrations

---
+++ database/migrations/2023_10_22_075849_create_choices_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('choices', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('question_bank_id')->constrained('question_banks');
+            $table->text('choice_text');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('choices');
+    }
+};
