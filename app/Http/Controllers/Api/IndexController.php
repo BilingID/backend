879d8c47e07972cb1f74f44d125723391f66a966
Naoby Prawira <naobyprawira8@gmail.com>
2023-12-22 10:03:49 +0700

    removing unused codes

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -10,20 +10,6 @@ use App\Models\Psychotest;
 
 class IndexController extends Controller
 {
-    public function index(Request $request)
-    {
-        if ($request->get('error') === 'true') {
-            return $this->error(null, 'coba', 417);
-        }
-
-        return $this->success([
-            'api' => config('app.name', 'Laravel'),
-            'version' => config('app.version', '1.0.0'),
-            'query' => $request->get('query'),
-            'adfadfasfdas' => $request->get('asdf'),
-        ]);
-    }
-
     public function statistic()
     {
         $users = User::all();
@@ -37,22 +23,5 @@ class IndexController extends Controller
             'total_counseling' => $counselings->count(),
         ]);
     }
-
-    public function show(Request $request, int $id)
-    {
-        $data = $request->json()->all();
-        $strReturn = $id . '. ' . $data['name'] . ' adalah kategori ';
-
-        if ($id === 10) {
-            return $this->success('Hello, Admin');
-        }
-
-        return $this->success($strReturn . match (true) {
-            $data['age'] < 15 => 'anak',
-            $data['age'] < 25 => 'remaja',
-            $data['age'] < 40 => 'dewasa',
-            $data['age'] < 60 => 'tua',
-            default => 'lansia',
-        });
     }
 }
